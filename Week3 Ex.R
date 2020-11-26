432a988a8c70b8cd2b811d0acfac535515ca2939
ZaraAl <74650605+ZaraAl@users.noreply.github.com>
2020-11-26 15:10:23 +0100

    Update Week3 Ex.R
    I tried :(

--- Week3 Ex.R
+++ Week3 Ex.R
@@ -1,15 +1,55 @@
+#WEEK THREE EXERCISE: ERRORS
+
 # Q1 ----------------------------------------------------------------------
 
+#converting the units 
 
+torad <- function(d, m, s) { rad <- pi* (d+m/60+s/3600/180)
+return(rad)}
 
+#the angle between the horizontal plane and the building to be 1°21’ 
+#the distance to a building is estimated from a map to be about 2550 25 m
 
+angle <- torad(1, 21, 0)
+angle_err <- torad(0, 1, 0)
+
+d <- 2550
+d_err <- 25
+
+#What is the height of the building and the fractional uncertainty in this estimate? 
+
+#equation is height = d*tan(angle)
+
+height <- d*tan(angle)
+print(height)
+
+height_err <- d_err*tan(angle_err)
+print(height_err)
+
+#uncertainty 
+
+un <- height_err/abs(height)
+print(un)
 
 # Q2 ----------------------------------------------------------------------
 
 
+#Volcano 1: 25.53 plus or minus 0.1 Ma
+#volcano 2: 29.66 plus or minus  0.2 
 
+v1 <- 25.53
+v1_err <- 0.1
+v2 <- 29.66
+v2_err <- 0.2
 
+#Duration of the volcanic activity? We use differences equation 
+#min x-y + delta x + delta y and min x-y-delta x - delta y
 
+max <- v1-v2+v1_err+v2_err
+min <- v1-v2-v1_err-v2_err
+
+print (min)
+print(max)
 
 
 # Q3 ----------------------------------------------------------------------
